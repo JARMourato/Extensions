ba5b2af86a954989b3c8585f6b3991f1e536c9ea
JARMourato <joao.armourato@gmail.com>
2022-12-05 13:48:52 -0800

    add color and point utilities

---
+++ Sources/CGPoint+Utilities.swift
@@ -0,0 +1,15 @@
+// Copyright © 2022 JARMourato All rights reserved.
+
+import CoreGraphics
+
+extension CGPoint {
+    /// Builds a point from an origin and a displacement
+    func displace(by point: CGPoint = .zero) -> CGPoint {
+        CGPoint(x: self.x+point.x, y: self.y+point.y)
+    }
+
+    /// Caps the point to the unit space
+    func capped() -> CGPoint {
+        CGPoint(x: max(min(x, 1), 0), y: max(min(y, 1), 0))
+    }
+}
---
+++ Sources/Color+Utilities.swift
@@ -0,0 +1,46 @@
+// Copyright © 2022 JARMourato All rights reserved.
+
+import SwiftUI
+
+// MARK: - Color from Hex
+extension Color {
+    init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 1.0
+
+        let length = hexSanitized.count
+
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
+
+        if length == 6 {
+            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+            b = CGFloat(rgb & 0x0000FF) / 255.0
+
+        } else if length == 8 {
+            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
+            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
+            a = CGFloat(rgb & 0x000000FF) / 255.0
+
+        } else {
+            return nil
+        }
+
+        self.init(red: r, green: g, blue: b, opacity: a)
+    }
+
+    init(_ hex: UInt32, opacity:Double = 1.0) {
+        let red = Double((hex & 0xff0000) >> 16) / 255.0
+        let green = Double((hex & 0xff00) >> 8) / 255.0
+        let blue = Double((hex & 0xff) >> 0) / 255.0
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+}
